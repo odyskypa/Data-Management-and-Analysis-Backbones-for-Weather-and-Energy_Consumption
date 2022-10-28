716312d75235cb47ed14741670147616f6666e76
Odysseas <odykypar@gmail.com>
2022-10-28 15:13:00 +0200

    Changes to be committed: 	modified:   databases_structures/databases_structure.py

--- databases_structures/databases_structure.py
+++ databases_structures/databases_structure.py
@@ -1,4 +1,5 @@
 from paths import dataBasesDir
+from utilities.db_utilities import getListOfTables
 import glob
 import duckdb
 
@@ -6,7 +7,10 @@ def diagnosis():
     for database_path in glob.iglob(dataBasesDir + '**/*.duckdb', recursive=True):
         print(f"\n Printing information for the {database_path} database: \n")
         con = duckdb.connect(database=database_path, read_only=False)
-        x = con.execute("DESCRIBE;")
-        print(x.description)
+        tables = getListOfTables(con)
+        print(tables)
+        for table in tables:
+            x = con.execute(f"DESCRIBE {table};")
+            print(x.description)
 
         
